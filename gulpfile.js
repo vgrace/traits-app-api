ba8673b50b8e4c265026cd23f96e128c351fd6d3
Vivian Esquivias <v.esquivias@gmail.com>
2016-10-17 11:02:01 +0200

    gulp added

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+﻿// grab our gulp packages
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	swagger = require('./tasks/swagger.js'),
+	watch = require('./tasks/watch.js'); 
+
+// create a default task and just log a message
+gulp.task('default', function () {
+	return gutil.log('Gulp is running!')
+});
